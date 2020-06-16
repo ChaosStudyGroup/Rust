16a15ef46cb48768f7dc04d182971e89e7775c4d
JGL666 <907599826@qq.com>
2020-06-16 22:55:41 +0800

    add c call rust

---
+++ ffi/simple_test_c_to_rust/src/lib.rs
@@ -0,0 +1,6 @@
+#[no_mangle]
+pub extern fn show(input: i32){
+
+    println!("input = {}",input);
+
+}
---
+++ ffi/simple_test_c_to_rust/src/main.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+
+#include <stdio.h>
+
+
+
+extern void show(int32_t input);
+
+
+
+int main() {
+
+    int input = 6;
+
+    show(input);
+
+
+    return 0;
+
+}
