e9f2e668809f6bec6f12ccd9c414ba4ceb870464
JGL666 <907599826@qq.com>
2020-06-17 22:01:03 +0800

    test rust c like string

--- ffi/simple_test_call_c/c_test/test.c
+++ ffi/simple_test_call_c/c_test/test.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 void show(char c){
 	printf("input : %d\n", c);
+}
+
+void show_str(char *c){
+	printf("input: %s\n",c);
+}
+
+void show_str_with_free(char *c){
+	printf("input: %s\n",c);
+	free(c);
+}
+
+char *get_str(){
+	return "hello world";
 }
--- ffi/simple_test_call_c/src/main.rs
+++ ffi/simple_test_call_c/src/main.rs
@@ -1,22 +1,41 @@
-  
-
-use libc;
-
-
+use libc::c_char;
+use std::ffi::CString;
+use std::ffi::CStr;
 
 extern {
-
-    fn show(input:  libc::c_char);
-
+    fn show(input: c_char);
+    fn show_str(intput: *const c_char);
+    fn show_str_with_free(input:* const c_char); 
+    fn get_str()->*const c_char;
 }
-
 
 
 fn main() {
 
     let input = 2;
+    let c_str = CString::new("123").unwrap();
 
-    unsafe { show(input) };
+    //let c_raw = c_str.into_raw();
+    unsafe { 
+        show(input);
+        
+        //read only
+       // show_str(c_str.as_ptr()); 
+        
+        //mem_leak
+        //show_str(c_str.into_raw()); 
+       
+        //c free no mem leak
+       // show_str_with_free(c_str.into_raw()); 
+       
+       // show_str(c_raw);
+       // let _ = CString::from_raw(c_raw);
+       
+        //println!("get str {:?}", CStr::from_ptr(get_str()));
+        println!("get str {}", CStr::from_ptr(get_str()).to_str().unwrap());
+
+                
+    };
 
 
 }
